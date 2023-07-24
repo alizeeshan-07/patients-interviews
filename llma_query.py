6b3cfc656478e55be1692f0c5a132d5a1cab3b29
Ali Zeeshan <70628992+alizeeshan-07@users.noreply.github.com>
2023-07-24 23:50:34 +0500

    second commit

--- llma_query.py
+++ llma_query.py
@@ -26,58 +26,63 @@ authenticator = stauth.Authenticate(names, usernames, hashed_passwords, "patient
 
 name, authentication_status, username = authenticator.login("Login", "main" )
 
+
+
 if authentication_status == False:
     st.error("Username/password is incorrect")
-    st.stop()
 
 if authentication_status == None:
     st.warning("Please enter your username and password")
-    st.stop()
 
-# Initializing session state variables after authentication
-if "chat_history" not in st.session_state:
-    st.session_state.chat_history = []
+if authentication_status:
+    if "chat_history" not in st.session_state:
+        st.session_state.chat_history = []
 
-if "input_key" not in st.session_state:
-    st.session_state.input_key = 0
+    if "input_key" not in st.session_state:
+        st.session_state.input_key = 0
+    
+    # Load documents
+    documents = SimpleDirectoryReader('sourcedata').load_data()
 
-# Load documents
-documents = SimpleDirectoryReader('sourcedata').load_data()
+    # Create the index
+    index = GPTVectorStoreIndex.from_documents(documents)
 
-# Create the index
-index = GPTVectorStoreIndex.from_documents(documents)
+    # Create the query engine
+    query_engine = index.as_query_engine()
 
-# Create the query engine
-query_engine = index.as_query_engine()
+    def handle_meta_question(question):
+        if question.lower() == 'what was my first question?':
+            for role, message in st.session_state.chat_history:
+                if role == "User":
+                    return message
+        # Add more conditions here to handle other types of meta-questions
+        return None
 
-def handle_meta_question(question):
-    if question.lower() == 'what was my first question?':
-        for role, message in st.session_state.chat_history:
-            if role == "User":
-                return message
-    # Add more conditions here to handle other types of meta-questions
-    return None
+    # Streamlit UI
+    
+    
+    st.sidebar.title(f"Welcome {name}!")
+    authenticator.logout("Logout", "sidebar")
 
-# Streamlit UI
-st.sidebar.title(f"Welcome {name}!")
-authenticator.logout("Logout", "sidebar")
 
-st.title("Experiences by Patient 'A' ")
 
-user_question = st.text_input("Ask the Patient's insights here", key=st.session_state.input_key)
 
-if user_question:
-    st.session_state.input_key += 1
-    meta_answer = handle_meta_question(user_question)
-    if meta_answer is not None:
-        result = meta_answer
-    else:
-        result = query_engine.query(user_question).response
-    st.session_state.chat_history.append(("User", user_question))
-    st.session_state.chat_history.append(("Patient", result))
+    st.title("Experiences by Patient 'A' ")
 
-for role, message in st.session_state.chat_history:
-    if role == "User":
-        st.markdown(f'**{role}**: {message}')
-    else:
-        st.markdown(f'**{role}**: {message}')
+    user_question = st.text_input("Ask the Patient's insights here", key=st.session_state.input_key)
+
+    if user_question:
+        st.session_state.input_key += 1
+        meta_answer = handle_meta_question(user_question)
+        if meta_answer is not None:
+            result = meta_answer
+        else:
+            result = query_engine.query(user_question).response
+        st.session_state.chat_history.append(("User", user_question))
+        st.session_state.chat_history.append(("Patient", result))
+
+    for role, message in st.session_state.chat_history:
+        if role == "User":
+            st.markdown(f'**{role}**: {message}')
+        else:
+            st.markdown(f'**{role}**: {message}')
