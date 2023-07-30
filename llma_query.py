afe26745ad76e374906b1dae0ca30be8a29f40e8
Ali Zeeshan <70628992+alizeeshan-07@users.noreply.github.com>
2023-07-30 18:17:14 +0500

    latest-30-07-2023

--- llma_query.py
+++ llma_query.py
@@ -4,7 +4,7 @@ from pathlib import Path
 import os
 from dotenv import load_dotenv
 import streamlit as st
-from llama_index import GPTVectorStoreIndex, SimpleDirectoryReader
+from llama_index import GPTVectorStoreIndex, SimpleDirectoryReader, Document
 import streamlit_authenticator as stauth
 
 st.set_page_config(page_title="Insights from Interviews")
@@ -32,9 +32,6 @@ authenticator = stauth.Authenticate(names, usernames, hashed_passwords, "patient
 
 name, authentication_status, username = authenticator.login("Login", "main" )
 
-
-
-
 if authentication_status == False:
     st.error("Username/password is incorrect")
 
@@ -43,9 +40,20 @@ if authentication_status == None:
 
 if authentication_status:
 
-    
-    # Load documents
-    documents = SimpleDirectoryReader('sourcedata').load_data()
+    # List all files in the 'sourcedata' directory
+    source_data_dir = 'sourcedata'
+    txt_files = [os.path.splitext(f)[0] for f in os.listdir(source_data_dir) if f.endswith('.txt')]
+
+    # Add selectbox to the sidebar
+    selected_file = st.sidebar.selectbox('Select the Person Name', txt_files)
+
+    # Load selected file (append '.txt' to the selected file before opening)
+    selected_file_path = os.path.join(source_data_dir, selected_file + '.txt')
+    with open(selected_file_path, 'r') as file:
+        text = file.read()
+
+    document = Document(text)  # creating Document object
+    documents = [document]
 
     # Create the index
     index = GPTVectorStoreIndex.from_documents(documents)
@@ -62,17 +70,13 @@ if authentication_status:
         return None
 
     # Streamlit UI
-    
-    
+
     st.sidebar.title(f"Welcome {name}!")
     authenticator.logout("Logout", "sidebar")
 
+    st.title("Healthcare Encounters")
 
-
-
-    st.title("Experiences by Patient 'A' ")
-
-    user_question = st.text_input("Ask the Patient's insights here", key=st.session_state.input_key)
+    user_question = st.text_input("Ask the person's insights here", key=st.session_state.input_key)
 
     if user_question:
         st.session_state.input_key += 1
